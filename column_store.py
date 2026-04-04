02b11b746af5de395a2a48e1ea3d77fef08f4e3d
RaeWinTan <raewintan@gmail.com>
2026-04-04 14:16:49 +0800

    first working version

--- column_store.py
+++ column_store.py
@@ -16,11 +16,12 @@ from typing import List, Any
 from collections.abc import Callable
 from enum import Enum
 from collections import defaultdict
-
+import csv 
 
 class DataType(Enum):
     STRING = 1
     INTEGER = 2
+    BIT = 3 
 
 
 class Writer:
@@ -28,30 +29,57 @@ class Writer:
     def __init__(self):
         self.pointer = 0
         # initialize work book to write as well
-
+    def setCsvWriter(self, file_name):
+        file = open(file_name, "w", newline="")
+        self.writer = csv.writer(file)
     def write(self, column_names, values):
-        pass
-        """
         if self.pointer ==0:
-            write the columns names and the values 
-        otherwise just write the values
-        """
+            self.writer.writerow(column_names)
+        self.writer.writerow(values)
+
+        self.pointer+=1     
 
 
-class ColumnObject:
+class ComparatorFunction:
+    
+    def __init__(self):
+        self.comparator_callbacks = {
+            "inRange": self.inRange, 
+            "isLesserThanEqualToUpperBound": self.isLesserThanEqualToUpperBound,
+            "isLesserThanUpperBound": self.isLesserThanUpperBound
+            }
+    def size(self):pass
+    def get(self, idx):
+        pass
+
+    def inRange(self, idx, rg):
+        pass 
+    
+    def isLesserThanEqualToUpperBound(self, idx, rg):
+        pass 
+    
+    def isLesserThanUpperBound(self, idx, rg):
+        pass 
+
+class ColumnObject(ComparatorFunction):
 
     def __init__(self, column_name, data_type: DataType):
+        super().__init__()
         self.column_name = column_name
         self.data_type = data_type
         self.data = []
         self.is_compressed = False
-
+    def inRange(self, idx, rg):
+        if self.data_type in [DataType.STRING, DataType.INTEGER]:
+            return rg[0]<=self.data[idx]<=rg[1]
+        return ((1<<self.data[idx]) & rg) > 0
     def size(self):
         return len(self.data)
 
     def load(self, value):
-        self.data.append(value)
-
+        if self.data_type==DataType.STRING:
+            self.data.append(value)
+        else:self.data.append(float(value))
     def reorder(self, permutation):
         rtn = self.data.copy()
         for i, p in enumerate(permutation):
@@ -66,6 +94,7 @@ class ColumnObject:
 
     def make_compress(self, hm):  # word -> integer,
         self.is_compressed = True
+        self.data_type = DataType.BIT
         self.hm = hm.copy()
         # the non intresting guys are just the last option
         self.ihm = defaultdict(lambda: len(self.hm)-1)
@@ -79,134 +108,38 @@ class ColumnObject:
         if self.is_compressed:
             return self.ihm[self.data[idx]]
         return self.data[idx]
-    
-    #function to be used by query to get he value 
 
-
-#check inside for columnObject 
-#if columnObject is aof type the check inside is just raneg 
-#if hte columnObject is compressed then it is check the bitmask
-#get min max for he zone map 
-
+class Indexes(ComparatorFunction):
+    def __init__(self, column_objects:List[ColumnObject], aggregate_fn):
+        #the feature we need is baisclaly check value kinda thing
+        #in this case check is mallsr
+        super().__init__()
+        self.column_objects = column_objects 
+        self.aggregate_fn = aggregate_fn
+    def size(self):
+        return self.column_objects[0].size()
+    def get(self, idx):
+        return self.aggregate_fn(*[c.get(idx) for c in self.column_objects])
+    def inRange(self, idx, rg):
+        return rg[0]<=self.get(idx)<=rg[1]
+    def isLesserThanEqualToUpperBound(self, idx, rg):
+        return self.get(idx)<=rg[1]
+    def isLesserThanUpperBound(self, idx, rg):
+        return self.get(idx)< rg[1]
 
 class ZoneMap:
-    SPAN = 16 #suppose page size is 16 objects long 
-    #aggregate function for compresse data is bitmask 
-    #aggreate function for normal number is just max and min
-    #need to have custom comparator, just check inside or not 
-    #aggregate function will give out like a range 
-    def __init__(self, column_objects:List[ColumnObject], aggregate_fn: Callable[[list[tuple[int, ...]]], Any], check_fn: Callable[[tuple[int, ...]], bool]):
-        self.size = column_objects[0].size()
+    SPAN = 16
+    #column objects is bsically iehter an index or a collumn objectd
+    def __init__(self, obj:ComparatorFunction, aggregate_fn: Callable[[list[tuple[int, ...]]], Any], check_fn: Callable[[tuple[int, ...]], bool]):
+        self.size = obj.size()
         self.zones = [None]*((self.size//self.SPAN) + 1)
         for l in range(0, self.size, self.SPAN):
             r = min(l+self.SPAN, self.size)
-            self.zones[l//self.SPAN] = aggregate_fn([(c.get(idx) for c in column_objects) for idx in range(l, r)])
+            self.zones[l//self.SPAN] = aggregate_fn([obj.get(idx) for idx in range(l, r)])
         self.check_fn = check_fn 
     def getZone(self, idx):
         return self.zones[idx//self.SPAN]
     def checkInside(self, idx, value):
         return self.check_fn(self.zones[idx//self.SPAN], value)
     def nextIdx(self, idx):#where detect a mis continue next indexi like that
-        return ((idx+self.SPAN)//self.SPAN)*self.SPAN 
-    
-
-
-class DataBase:
-    
-    def __init__(self, column_names, data_types):
-        self.column_stores = dict()
-        for i, name in enumerate(column_names):
-            self.column_stores[name] = ColumnObject(name, data_types[i])
-        self.writer = Writer()
-        self.size = 0 
-        self.indexes = []
-        self.zone_maps = dict()
-
-    def compress_column(self, column_name, hm):
-        self.column_stores[column_name].make_compress(hm)
-
-    def load_data(self, data):#data is a dicionary for each column the values in i
-        assert set(data.keys())==set(self.column_stores.keys()), "All columns must match columns in database"
-        self.size += 1 
-        for name, v in data.items():
-            cs:ColumnObject = self.column_stores[name]
-            cs.load(v)
-
-    def index_columns(self, indexes:List[List[str]], aggregate_fns):
-        arr = []
-        self.indexes = [v.copy() for v in indexes]
-        for i in range(self.sz):
-            for ic,cols in enumerate(indexes):
-                v = []
-                for c in cols:
-                    col_obj:ColumnObject = self.column_stores[c]
-                    v.append(col_obj.get(i))
-                v = tuple(v)
-                arr.append((aggregate_fns[ic](*v), i))
-        arr.sort(key=lambda x: x[0])#order by the value defined in the tuple 
-        permutation = [i for _,i in arr]
-        for (_,col_obj) in self.column_stores.items():
-            col_obj.reorder(permutation)        
-    
-    def zone_map_columns(self, zones:List[tuple[str,...]], aggregate_fns, check_fns):
-        self.zone_maps
-        #same tupple idea
-        for i,z in enumerate(zones):
-            self.zone_maps[z] = ZoneMap([self.column_stores[c] for c in z], 
-                                        aggregate_fns[i],
-                                        check_fns[i]
-                                        )
-
-    def write_data(self, row_number, columns, aggregation_column_names,aggregation_columns, aggregation_functions):
-        values = [] 
-        for name in columns:
-            cs:ColumnObject = self.column_stores[name]
-            values.append(cs.get_original_value(row_number)) 
-        for i in range(len(aggregation_functions)):
-            fn = aggregation_functions[i]
-            col_names = aggregation_columns[i]
-            col_values = [self.column_stores[nm].get_original_value(row_number) for nm in col_names]
-            values.append(fn(*col_values))
-        self.writer.write(columns+aggregation_column_names, values)
-
-    def query(self, x, y, mat_id):#query either give back -1 or a row_number, -1 means no result
-        DIGIT_TO_TOWN = {
-            '0': 'BEDOK',
-            '1': 'BUKIT PANJANG',
-            '2': 'CLEMENTI',
-            '3': 'CHOA CHU KANG',
-            '4': 'HOUGANG',
-            '5': 'JURONG WEST',
-            '6': 'PASIR RIS',
-            '7': 'TAMPINES',
-            '8': 'WOODLANDS',
-            '9': 'YISHUN'
-        }
-        DIGIT_TO_YEAR = {
-            '5': 2015, '6': 2016, '7': 2017, '8': 2018, '9': 2019,
-            '0': 2020, '1': 2021, '2': 2022, '3': 2023, '4': 2024
-        }
-        def decode_matric(matric):
-            """
-            Given a matric number like U2340246H, extract:
-            - target year   (last digit of number portion)
-            - start month   (second last digit; '0' = October)
-            - towns         (all unique digits in matric)
-            """
-            # Extract only the digit characters
-            digits = [c for c in matric if c.isdigit()]
-
-            last_digit        = digits[-1]   # target year
-            second_last_digit = digits[-2]   # start month
-
-            target_year  = DIGIT_TO_YEAR[last_digit]
-            start_month  = 10 if second_last_digit == '0' else int(second_last_digit)
-            towns        = list(dict.fromkeys(  # unique digits, preserve order
-                            DIGIT_TO_TOWN[d] for d in digits if d in DIGIT_TO_TOWN
-                        ))
-            return target_year, start_month, towns
-        start_year, start_month, towns = decode_matric(mat_id)
-        #time span over here (start_year, start_month) + x 
-        #binary search starts and ends 
-        #binary 
-        #now that that is started 
+        return min(((idx+self.SPAN)//self.SPAN)*self.SPAN, self.size)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from column_store import DataBase, DataType
+from database import DataBase
+from column_store import DataType
 from collections import defaultdict
+from common_utils import MinMax, haveOverLap, inSet, bit_encoded
 
 town_to_digit = defaultdict(lambda: 10, {
     "BEDOK": 0,
@@ -13,93 +15,53 @@ town_to_digit = defaultdict(lambda: 10, {
     "WOODLANDS": 8,
     "YISHUN": 9
 })
-column_names = ["Year",           "Month",          "Town",         "Block",        "Street_Name",  "Flat_Type",    "Flat_Model",   "Storey_Range", "Floor_Area",    "Lease_Commence_Date", "Resale_Price"]
+column_names = ["year",           "month",          "town",         "block",        "street_name",  "flat_type",    "flat_model",   "storey_range", "floor_area_sqm",    "lease_commence_date", "resale_price"]
 column_types = [DataType.INTEGER, DataType.INTEGER, DataType.STRING,DataType.STRING,DataType.STRING,DataType.STRING,DataType.STRING,DataType.STRING,DataType.INTEGER,DataType.STRING,DataType.INTEGER] 
 #must clean up the month and yesr part simple split will do 
-def MinMax(arr: list[tuple[int, ...]]):
-    mn,mx = arr[0],arr[0]
-    for i in range(1, len(arr)):
-        mn = min(arr[i], mn)
-        mx = max(arr[i], mx)
-    return (mn, mx)
 
-def inRange(z, val):
-    return z[0]<=val<=z[1]
-def inSet(z, val):
-    return (z & val)!=0 
-def bit_encoded(arr):
-    acc = 0 
-    for (a) in arr:
-        acc|=(1<<a)
-    return acc 
 
 db = DataBase(column_names, column_types)
 #read. form csv then load 1 at a time 
 #for loop to loade data readhing from csv until end of file 
-db.load_data()
-db.compress_column("Town", town_to_digit)
-db.index_columns([["Year", "Month"], ["Resale_Price","Floor_Area"]], [lambda a,b: (a,b), lambda a,b: (a*1.0)/b])
-db.zone_map_columns([("Year", "Month"), ("Floor_Area"), ("Town")], [MinMax, MinMax, bit_encoded], [inRange, inRange, inSet])
+import csv
 
-##queries here
-
-
-
-"""
-
-check in range 
-zonemap columns
-(year, month), (town), (area)
-initialize db 
-
-get data from the csv file
-
-load_data to db 
-
-compress_column 
-
-index_column 
-
-zone_map_columns
-
-queries : declarative code using rxpy
-
-
-if is compressed-> bit manipulation for zonemap
-if indexed -> normal range find 
-
-area >= 85 
-
-early stopping 
-
-as long as i find an answer in the year
-month(this is because the price/area is 
-sorted within the year,month)  
-
-all those zones with the same year month 
-can be skipped 
-
-still need to vectorize ? 
-
-for hte zone map part no need lah 
-
-how to vectorize?
-
-index by (year,month), price/floor_area
-
-SHOULD ZONE MAP YEAR MONTH-> kicker
--> if i found the 
-binary search (year, month)
-within that span 
-search through using zone map
-area and town
-shared scan area and town and price/floor_area
-if all three zones hit: scan it 
-    -> the first actual hit 
-    -> update he answer
-    -> skip all the way to the next year,mont
-        -> basically check the getZone for year,month
-        -> if start and end date the same then continue 
+with open("ResalePricesSingapore.csv", newline="") as f:
+    reader = csv.reader(f)
+    
+    headers = next(reader)  # first row
+    month_map = {
+    "Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4,
+    "May": 5, "Jun": 6, "Jul": 7, "Aug": 8,
+    "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12
+    }
+    for row in reader:
+        row_dict = dict(zip(headers, row))
+        month_year = row_dict["month"]
+        del row_dict["month"]
+        mon_str, yy = month_year.split("-")
+        row_dict["month"] = month_map[mon_str]
+        row_dict["year"] = "20"+yy
+        db.load_data(row_dict)
+def normalize_number(val):
+    f = float(val)
+    return int(f) if f.is_integer() else f
+def month_fill(val):
+    val = int(val)
+    return f"{val:02d}"
+db.compress_column("town", town_to_digit)
+db.index_columns([("year", "month"), ("resale_price","floor_area_sqm")], [lambda a,b: (a,b), lambda a,b: ((a*1.0)/b)])
+#need to change index, index, coloum_object, column_objects
+db.zone_map_columns([("year", "month"), ("resale_price", "floor_area_sqm"),("floor_area_sqm"), ("town")], [MinMax, MinMax,MinMax, bit_encoded], [haveOverLap, haveOverLap, haveOverLap, inSet])
 
 
-"""
+db.writer.setCsvWriter("ScanResult_U2340246H_rw.csv")
+for x in range(1,9):
+    for y in range(80,151):
+        rtn = db.query(x, y, "U2340246H")
+        if rtn!=-1:
+            db.write_data( x,y,rtn,["year", "month", "town", "block", "floor_area_sqm", "flat_model", "lease_commence_date"],
+                        ["Price_Per_Square_Meter"], [["resale_price", "floor_area_sqm"]], [lambda a,b: (a*1.0)/b],[int, month_fill, str, str, normalize_number, str, str,round],
+                        ["(x, y)","Year","Month","Town", "Block","Floor_Area", "Flat_Model", "Lease_Commence_Date"	,"Price_Per_Square_Meter"])
+
+
+
